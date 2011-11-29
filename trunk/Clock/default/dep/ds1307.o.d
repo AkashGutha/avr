2976e66771ce5116c9c56bd512dd912d7c97b6b1
kovasznaiistvan@gmail.com <kovasznaiistvan@gmail.com@f96d22c5-9466-f0a8-b727-92a628e432d3>
2011-11-29 12:05:11 +0000

    ATmega88 projects

---
+++ trunk/Clock/default/dep/ds1307.o.d
@@ -0,0 +1,36 @@
+ds1307.o: ../ds1307.c \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr\io.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/stdint.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/iom88.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/iomx8.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20090313/lib/gcc/../../avr/include/avr/lock.h ../ds1307.h
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr\io.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/stdint.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/iom88.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/iomx8.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr-20090313/lib/gcc/../../avr/include/avr/lock.h:
+
+../ds1307.h:
---
+++ trunk/Thermostat/Nokia 3310/Model/readme.txt
@@ -0,0 +1,3 @@
+copy this 2 folders to 
+c:\Program Files\Labcenter Electronics\Proteus 7 Professional\*.*
+works fine on all versions
